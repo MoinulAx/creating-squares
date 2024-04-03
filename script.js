8d78438cb236fd7801ae80a2ddbe5d73345a8c4b
MoinulAx <moinulkhan347@gmail.com>
2024-04-03 12:56:01 -0400

    hjbhjb

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const enterButton = document.getElementById("enterButton")
 const colorArray =  ["#EA0B0B", "#6540FC", "#63AECE", "#D16FDC", "#238100", "#FEFF00", "#FF7B00", "#0055FF", "#1C1C2E", "#FFA9A3", "#F88FFF"];
 const boxes = document.getElementsByTagName('div')
+const mainContainer = document.getElementsByTagName('main')
 
 
 
 enterButton.addEventListener('click', (event) =>{
     const colorItems = document.createElement('div')
     colorItems.setAttribute('class','container__items')
-    boxes.styles.border = colorArray[Math.floor(Math.random()*colorArray.length)]
+    colorItems.style.borderColor = colorArray[Math.floor(Math.random()*colorArray.length)]
+    if(mainContainer.length === 0){
+        mainContainer.append(colorItems)
+    }else{
+        colorItems.appendChild(colorItems)
+    }
 
 
 
