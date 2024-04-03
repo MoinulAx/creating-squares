6d11466f27d0b316aef46c0c562954dc4cef2555
MoinulAx <moinulkhan347@gmail.com>
2024-04-03 16:03:11 -0400

    Final

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const enterButton = document.getElementById("enterButton")
 const colorArray =  ["#EA0B0B", "#6540FC", "#63AECE", "#D16FDC", "#238100", "#FEFF00", "#FF7B00", "#0055FF", "#1C1C2E", "#FFA9A3", "#F88FFF"];
 const boxes = document.getElementsByTagName('div')
-const mainContainer = document.getElementsByTagName('main')
+const mainContainer = document.querySelector('main')
 
 
 
 enterButton.addEventListener('click', (event) =>{
     const colorItems = document.createElement('div')
-    colorItems.setAttribute('class','container__items')
+    colorItems.setAttribute('class','items')
     colorItems.style.borderColor = colorArray[Math.floor(Math.random()*colorArray.length)]
-    if(mainContainer.length === 0){
+    if(boxes.length === 0){
         mainContainer.append(colorItems)
-    }else{
-        colorItems.appendChild(colorItems)
+    }else {
+        boxes[boxes.length-1].append(colorItems)
+        if(boxes.length === 19){
+            mainContainer.removeChild(boxes[0])  
+        }
     }
+
+
 
 
 
